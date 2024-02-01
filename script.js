efa380ceae6f02d3d099d2789ec31c4cc01b7888
Magdaléna Martan Kohlbergerová <magdalenaa.martankohlbergerova@gmail.com>
2024-02-01 21:10:24 +0100

    work section

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-bar').onclick=() =>{
     navbar.classList.toggle('active')
 };
+
+const initSlider = () => {
+    const imageList = document.querySelector (".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".slider-container .slider-scrollbar");
+    const scrollbarThumb = document.querySelector(".scrollbat-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy ({ left: scrollAmount, behaviour: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    };
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons()
+    });
+}
+
+window.addEventListener("load", initSlider)
